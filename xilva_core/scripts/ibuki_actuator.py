4b433c0de1bd4107a66404bdf688a8fe85f18b19
ustyui <45918907+ustyui@users.noreply.github.com>
2019-11-25 18:30:50 +0900

    Add files via upload

--- xilva_core/scripts/ibuki_actuator.py
+++ xilva_core/scripts/ibuki_actuator.py
@@ -3,37 +3,31 @@
 import rospy
 from xilva_core.msg import EvansString
 
-import socket, errno, sys, os
+import socket, errno, sys
 
 import modules.utils as utils
 import modules.logging as logs
 
+dev_name = sys.argv[1]
 
 _RATE = 40
 
 class Joint():
-    def __init__(self):
-        self.payloads = {'neck': '',
-                         'arml': '',
-                         'armr': '',
-                         'handl': '',
-                         'handr': '',
-                         'headl': '',
-                         'headc': '',
-                         'headr': '',
-                         'hip': '',
-                         'wheel':''}
+    def __init__(self, name = 'void'):
+        self.name = name
+        self.payload = []
 
 def callback(msg, args):
     instance = args
-    instance.payloads[msg.name] = msg.payload
+    if instance.name == dev_name:
+        instance.payloads[msg.name] = msg.payload
 "socket initialization"
 "a subscriber to receive and send"
 "get ip, port"
 if __name__ == "__main__":
     
     motorsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    nh = rospy.init_node('ibuki_actuators', anonymous = True)
+    nh = rospy.init_node('ibuki_actuators_'+dev_name, anonymous = True)
     param_config = utils.read_yaml('xilva_core', 'ibuki')
     
     _IP = param_config['IP']
@@ -45,15 +39,15 @@ if __name__ == "__main__":
     sub = rospy.Subscriber('/ibuki/encoded_commands', EvansString, callback, mbed_joint)
     
     while not rospy.is_shutdown():
-        for elements in mbed_joint.payloads:
-            embed_msg = mbed_joint.payloads[elements]
-            try:
-                motorsock.sendto(embed_msg, (_IP[elements], _PORT[elements]))
-            except socket.error as error:
-                if error.errno == errno.ENETUNREACH:
-                    rospy.logfatal_once(logtext.report_log(99))
-                else:
-                    raise
+        
+        embed_msg = mbed_joint.payload
+        try:
+            motorsock.sendto(embed_msg, (_IP[dev_name], _PORT[dev_name]))
+        except socket.error as error:
+            if error.errno == errno.ENETUNREACH:
+                rospy.logfatal_once(logtext.report_log(99))
+            else:
+                raise
         rate.sleep()
             
                 
