9d82e2a613c0f3f3c73fe053fdd094bd56cf1f05
ustyui <45918907+ustyui@users.noreply.github.com>
2019-12-03 19:36:45 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ INSTALL_DIR=$PWD
 sudo -H chmod -R u+x $INSTALL_DIR/src
 sudo -H chmod -R u+x $INSTALL_DIR/src/modules
 sudo -H chmod -R u+x $INSTALL_DIR/scripts
+sudo -H apt-get install python-pandas -y
+
 echo "${GREEN}${bold}---The xilva is installed!---${NC}" 
